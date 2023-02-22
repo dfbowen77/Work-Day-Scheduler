a0bdbf324f48b70ba45529e6c44eaf20202bc0a3
Dan Bowen <danbowen2677@gmail.com>
2023-02-22 11:43:19 -0500

    Date displays and css and js files are linked in HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+var now = dayjs().format('dddd, MMMM D, YYYY h:mm A')
+$('#currentDay').text(now);
 $(function () {
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
